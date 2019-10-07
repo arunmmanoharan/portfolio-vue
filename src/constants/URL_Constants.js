e9329e14c2822259cd39d03dccd9e7eb8e1838f4
Arun Manohar <arunmanohar.manoharan@cms.hhs.gov>
2019-10-07 15:35:04 -0400

    Add redirect buttons

--- src/constants/URL_Constants.js
+++ src/constants/URL_Constants.js
@@ -1,4 +1,5 @@
 const API = 'https://arun-manohar-resume-api.herokuapp.com/'
 export const ANGULAR_URL = 'https://arun-manohar-portfolio-angular.herokuapp.com'
+export const REACT_URL = 'https://arun-manohar-resume.herokuapp.com/';
 
 export default API
